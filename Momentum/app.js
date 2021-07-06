e52c3c10018b98a5656d2d6501da76b1f6a02eda
dasssseul <daseul8086@gmail.com>
2021-07-06 13:48:47 +0900

    study vanilla js

---
+++ Momentum/app.js
@@ -0,0 +1,13 @@
+const a = 5;
+const b = 2;
+
+let myName = "daseul";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("Hello " + myName);
+
+myName = "ds";
+
+console.log("your new name is " + myName);
