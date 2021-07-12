2e7c74d63ca5e65dbdfa9d2c9645234e5a975add
dasssseul <daseul8086@gmail.com>
2021-07-12 21:37:22 +0900

    study vanilla js 2.4	~ 2.12

--- Momentum/app.js
+++ Momentum/app.js
@@ -1,3 +1,8 @@
+
+// [노마드코더] 바닐라 JS로 크롬 앱 만들기
+
+// 2.3 const and let
+
 const a = 5;
 const b = 2;
 
@@ -11,3 +16,110 @@ console.log("Hello " + myName);
 myName = "ds";
 
 console.log("your new name is " + myName);
+
+// 2.4 Booleans
+
+const amIFat = null;
+let something;
+console.log(amIFat); // null : 비어있다
+console.log(something); // undefined : 존재하지만, 안에 값이 없다
+
+// 2.5 Arrays
+
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"]
+
+console.log(daysOfWeek);
+
+// get item fro array
+console.log(daysOfWeek[4]);
+
+// add one more day to the array
+daysOfWeek.push("sun");
+
+console.log(daysOfWeek);
+
+// 2.6 Objects
+
+const player = {
+    name: "daseul",
+    points: 10,
+    fat: false,
+};
+console.log(player);
+console.log(player.name);
+console.log(player["points"]);
+
+// 수정
+player.fat = true;
+console.log(player);
+
+// 추가
+player.lastName = "potato";
+console.log(player);
+
+// 2.7~8 Functions
+
+function sayHello(nameOfPerson, age){
+    console.log("Hello my name is " + nameOfPerson + " and I'm " + age);
+};
+
+sayHello("nico", 10);
+sayHello("daseul", 20);
+
+const player2 = {
+    name: "daseul",
+    sayHello : function(otherName){
+        console.log("hello " + otherName + " nice to meet you!")
+    }
+};
+
+player2.sayHello("nico");
+
+function plus(a, b){
+    console.log(a+b);
+}
+
+function divide(a, b){
+    console.log(a/b);
+}
+
+plus(1, 2);
+divide(1, 2);
+
+// 2.9~10 Recap
+
+// 계산기 만들기
+
+const calculator = {
+    plus : function (a, b){
+        return a+b;
+    },
+    
+    minus : function (a, b){
+        return a-b;
+    },
+
+    divide : function (a, b){
+        return a/b;
+    },
+
+    powerOf: function (a, b){
+        return a**b;
+    }
+
+}
+
+console.log(calculator.plus(2, 5));
+console.log(calculator.minus(2, 5));
+console.log(calculator.divide(2, 5));
+console.log(calculator.powerOf(2, 5));
+
+// 2.11 Returns
+
+const age = 96;
+function calculateKrAge(ageOfForeigner){
+    return ageOfForeigner + 2;
+}
+
+console.log(calculateKrAge(age));
+
